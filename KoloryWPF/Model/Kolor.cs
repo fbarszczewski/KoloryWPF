f7029e6b84b7c0f5ab5abbaa23cb096814f467af
Filip Barszczewski <f.barszczewski@qt-swiss.eu>
2023-02-04 16:52:02 +0100

    added DAL

--- KoloryWPF/Model/Kolor.cs
+++ KoloryWPF/Model/Kolor.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace KoloryWPF.Model
 {
-    internal class Kolor
+    public class Kolor
     {
+        public byte R { get; set; }
+        public byte G { get; set; }
+        public byte B { get; set; }
+        public Kolor(byte r, byte g, byte b)
+        {
+            this.R = r;
+            this.G = g;
+            this.B = b;
+        }
     }
+
+    static class Ustawienia
+    {
+        public static Kolor Czytaj()
+        {
+            Properties.Settings ustawienia = Properties.Settings.Default;
+            return new Kolor(ustawienia.R, ustawienia.G, ustawienia.B);
+        }
+        public static void Zapisz(Kolor kolor)
+        {
+            Properties.Settings ustawienia = Properties.Settings.Default;
+            ustawienia.R = kolor.R;
+            ustawienia.G = kolor.G;
+            ustawienia.B = kolor.B;
+            ustawienia.Save();
+        }
+    }
+
 }
