bb5b613d2705c5bea10bdedd1ef84018350576ea
Filip Barszczewski <f.barszczewski@qt-swiss.eu>
2023-02-04 16:50:11 +0100

    Dodaj pliki projektów.

---
+++ KoloryWPF/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KoloryWPF
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Color kolor = Ustawienia.Czytaj();
+            rectangle.Fill = new SolidColorBrush(kolor);
+            sliderR.Value = kolor.R;
+            sliderG.Value = kolor.G;
+            sliderB.Value = kolor.B;
+        }
+
+        private Color KolorProstokąta
+        {
+            get
+            {
+                return (rectangle.Fill as SolidColorBrush).Color;
+            }
+            set
+            {
+                (rectangle.Fill as SolidColorBrush).Color = value;
+            }
+        }
+        private void sliderR_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Color kolor = Color.FromRgb(
+            (byte)sliderR.Value,
+            (byte)sliderG.Value,
+            (byte)sliderB.Value);
+            KolorProstokąta = kolor;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape) Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Ustawienia.Zapisz(KolorProstokąta);
+        }
+    }
+
+}
---
+++ KoloryWPF/Ustawienia.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace KoloryWPF
+{
+    static class Ustawienia
+    {
+        public static Color Czytaj()
+        {
+            Properties.Settings ustawienia = Properties.Settings.Default;
+            Color kolor = new Color()
+            {
+                A = 255,
+                R = ustawienia.R,
+                G = ustawienia.G,
+                B = ustawienia.B
+            };
+            return kolor;
+        }
+        public static void Zapisz(Color kolor)
+        {
+            Properties.Settings ustawienia = Properties.Settings.Default;
+            ustawienia.R = kolor.R;
+            ustawienia.G = kolor.G;
+            ustawienia.B = kolor.B;
+            ustawienia.Save();
+        }
+
+    }
+}
