f9681ba9e9fba6dcfae21c7d339e4019d21f328d
Filip Barszczewski <f.barszczewski@qt-swiss.eu>
2023-02-04 21:59:01 +0100

    added converter for sliders

---
+++ KoloryWPF/Konwertery.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace KoloryWPF
+{
+    public class ByteToDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter,
+        System.Globalization.CultureInfo culture)
+        {
+            return (double)(byte)value;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter,
+        System.Globalization.CultureInfo culture)
+        {
+            return (byte)(double)value;
+        }
+
+    }
+
+
+}
